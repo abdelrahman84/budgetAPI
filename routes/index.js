bd4c1c42f098e580784fe8e7e8c247d39ca2e2a6
developerNinja <a.farrag@zinad.net>
2020-04-21 00:32:28 +0200

    adding login endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
+let bcrypt = require('bcrypt');
 
 var User = mongoose.model('Users');
 
-var crypto = require('crypto'), hmac, signature;
-
 const { check, validationResult } = require('express-validator');
-
-const { matchedData, sanitize } = require('express-validator');
 
 let jwt = require('jsonwebtoken');
 
@@ -21,6 +18,41 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post('/login', [
+  check('password', 'password can`t be left blank').isLength({ min: 1 }),
+  check('email').isEmail().withMessage('Please enter a valid email address').trim(),
+], function (req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ message: errors.array() });
+  } else {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email }).then(
+      user => {
+        if (!user) {
+          return res.status(400).json({ error: 'user doesn`t exist' })
+        }
+        bcrypt.compare(password, user.password)
+          .then((isMatch) => {
+            if (!isMatch) {
+              return res.status(400).json({ error: 'incorrect password, please check and try again' })
+            } if (isMatch) {
+              let token = jwt.sign({ username: user.username },
+                config.secret,
+                {
+                  expiresIn: '24h'
+                })
+              res.json({ status: 'login success', user, token: token });
+            }
+          })
+      }
+    )
+
+  }
+});
 
 router.post('/register', [
   check('first_name', 'First name can`t be left blank').isLength({ min: 1 }),
@@ -40,45 +72,42 @@ router.post('/register', [
   if (!errors.isEmpty()) {
     res.json({ status: 'error', message: errors.array() });
   } else {
-    hmac = crypto.createHash('sha1', 'auth secret');
-    var encPassword = '';
+  
 
-    if (req.body.password) {
-      hmac.update(req.body.password);
-      encPassword = hmac.digest('hex');
-    }
-    var data = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      username: req.body.username,
-      password: encPassword
-    };
-    let token = jwt.sign({username: req.body.username},
-      config.secret,
-      {
-        expiresIn: '24h'
-      }) 
-    var user = new User(data);
-    user.save(function (error) {
-      console.log(user);
-      if (error) {
-        throw error;
-      }   
-      res.json({ status: 'success', user, token: token });
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+      var data = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        username: req.body.username,
+        password: hash
+      };
+      let token = jwt.sign({ username: req.body.username },
+        config.secret,
+        {
+          expiresIn: '24h'
+        })
+      var user = new User(data);
+      user.save(function (error) {
+        console.log(user);
+        if (error) {
+          throw error;
+        }
+        res.json({ status: 'success', user, token: token });
+      });
     });
   }
 });
 
 function findUserByEmail(email) {
-  if(email) {
+  if (email) {
     return new Promise((resolve, reject) => {
-      User.findOne({email: email})
-       .exec((err, doc) => {
-         if (err) return reject(err)
-         if (doc) return reject(new Error('This email already exists. Please enter another email.'))
+      User.findOne({ email: email })
+        .exec((err, doc) => {
+          if (err) return reject(err)
+          if (doc) return reject(new Error('This email already exists. Please enter another email.'))
           else return resolve(email)
-       })
+        })
     })
   }
 
